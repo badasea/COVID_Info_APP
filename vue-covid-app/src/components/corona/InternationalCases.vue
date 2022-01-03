3853d1f4673939f1300cdbf39521048a9bba63cd
jungbada269 <57929751+jungbada269@users.noreply.github.com>
2022-01-03 17:05:10 +0900

    axios test(issue O)

--- vue-covid-app/src/components/corona/InternationalCases.vue
+++ vue-covid-app/src/components/corona/InternationalCases.vue
@@ -5,8 +5,20 @@
 </template>
 
 <script>
+import coronaMixin from "@/mixins/coronaMixin";
 export default {
   name: "InternationalCases",
+  mixins: [coronaMixin],
+  mounted() {
+    this.fetchCases();
+  },
+  methods: {
+    async fetchCases() {
+      const url = "https://api.covid19api.com/summary";
+      const summary = await this.fetchData("get", url);
+      console.log(summary, "res");
+    },
+  },
 };
 </script>
 
